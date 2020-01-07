12aa4e09ae7d17dd06052ad1fa7aad540a8aa847
Rémy <Remypottierpro@gmail.com>
2020-01-07 15:48:40 +0100

    Change for server

--- server.js
+++ server.js
@@ -1,16 +1,43 @@
 require('dotenv').config();
+
+const { Pool } = require('pg');
 const express = require('express');
 
 const app = express();
-const constructionSites = require('./routes/constructionSites');
-
+const port = 4000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const port = process.env.PORT || 4000;
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.PORTPG,
+});
 
+app.get('/api/v1/construction-sites', (req, res) => {
+  pool.query('SELECT * from construction_sites', (err, results) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    return res.send(results.rows);
+  });
+});
 
-app.use('/api/v1/construction-sites', constructionSites);
+app.post('/api/v1/construction-sites', (req, res) => {
+  const { name, coords } = req.body;
+  pool.query(
+    'INSERT INTO construction_sites (name,coords) VALUES ($1, $2)',
+    [name, coords],
+    (err, results) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+      return res.send(results);
+    },
+  );
+});
 
 app.listen(port, (err) => {
   if (err) {
