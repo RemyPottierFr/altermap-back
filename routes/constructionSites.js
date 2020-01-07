00c224307d5defec376da9e07764dcef0ce0f58c
Rémy <Remypottierpro@gmail.com>
2020-01-07 16:18:59 +0100

    Add put and delete route

--- routes/constructionSites.js
+++ routes/constructionSites.js
@@ -26,4 +26,35 @@ router.post('/', (req, res) => {
   );
 });
 
+router.put('/api/v1/construction-sites/:id', async (req, res) => {
+  const { id } = req.params;
+  const { name, coords } = req.body;
+  console.log(req.body);
+  await pool.query('UPDATE construction_sites SET coords =$3, name = $1  WHERE id =$2',
+    [name, id, coords],
+    (err) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Erreur lors de la modification d'un chantier");
+      } else {
+        res.sendStatus(200);
+      }
+    });
+});
+router.delete('/api/v1/construction-sites/:id', async (req, res) => {
+  const { id } = req.params;
+  console.log(req.body);
+  await pool.query('DELETE FROM construction_sites where id= $1',
+    [id],
+    (err) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Erreur lors de la suppression d'un chantier");
+      } else {
+        res.sendStatus(200);
+      }
+      console.log(id);
+    });
+});
+
 module.exports = router;
